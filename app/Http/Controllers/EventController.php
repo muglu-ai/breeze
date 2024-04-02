8457dee11a218c04d0c5e4ac9bf04625fed46e81
Manish Kumar Sharma <manishk_sharma@outlook.com>
2024-04-03 01:05:18 +0530

    register,login, middleware, profile, event update

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Http\Requests\EventUpdate;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Log;
+
+
+
+class EventController extends Controller
+{
+    //
+    /**
+     * Update your event Details form.
+     */
+    public function edit(Request $request): View
+    {
+        return view('event_details.edit', [
+            'user' => $request->user(),
+        ]);
+    }
+    public function update(EventUpdate $request): RedirectResponse
+    {
+        $request->user()->fill($request->validated());
+
+        Log::info($request->validated());
+
+        if ($request->user()->isDirty('email')) {
+            //insert into database
+            $request->user()->email_verified_at = "25/02/2024";
+        }
+
+        $request->user()->save();
+
+        return Redirect::route('event.edit')->with('status', 'event-updated');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EventController;
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/event_update', [EventController::class, 'edit'])->name('event.edit');
+    Route::patch('/event_update', [EventController::class, 'update'])->name('event.update');
+
+});
+
+
+require __DIR__.'/auth.php';
